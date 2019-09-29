9be53c79bdc4fbc385fc5ce2f495ebc0faf4f04f
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-29 07:20:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,9 +20,9 @@ def monopoly_with_third_tier
   # complexity.
   monopoly_with_second_tier[:railroads][:rent_in_dollars]={
     one_piece_owned:
-    one_piece_owned:
-    one_piece_owned:
-    one_piece_owned:
+    two_piece_owned:
+    three_piece_owned:
+    four_piece_owned:
   }
 end
 

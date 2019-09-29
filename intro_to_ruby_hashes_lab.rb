8ee85dcace3e34e763faf3c74e43c204040939de
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-29 07:49:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ def monopoly_with_second_tier
   # complexity.
   
   base_hash[:railroads][:pieces] = 4
-  puts base_hash
+  puts base_hash[:railroads][:pieces] 
 end
 
 def monopoly_with_third_tier

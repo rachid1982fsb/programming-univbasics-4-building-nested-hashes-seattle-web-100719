6b85920c725cbdaaf9aad00be436e96435b19749
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-29 07:52:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,8 +10,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  base_hash[:railroads][:pieces] = 4
-  return base_hash
+  base_hash={:railroads =>{:pieces => 4}}
+  
 end
 
 def monopoly_with_third_tier

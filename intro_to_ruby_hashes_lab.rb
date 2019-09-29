f3e09b96b12e01ba263de15ca9a89f6ccc9d0846
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-29 08:04:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,8 @@ def monopoly_with_third_tier
     three_piece_owned: 100,
     four_piece_owned: 200
   }
-  have[:railroads][:names]={
+  
+  hash[:railroads][:names]={
     reading_railroad: {},
     pennsylvania_railroad: {},
     b_and_o_railroad: {},

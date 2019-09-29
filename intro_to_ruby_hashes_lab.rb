62579a3ec1e990c1dc54a752d1d8c791166f89a1
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-29 08:12:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -54,5 +54,5 @@ def monopoly_with_fourth_tier
 end
 puts monopoly_with_second_tier
 puts monopoly_with_third_tier[:railroads][:names][reading_railroad]
-puts monopoly_with_fourth_tier
+#puts monopoly_with_fourth_tier
 
